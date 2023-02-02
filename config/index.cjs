44c691083a2ed91a248cb97c91700061740dd90a
ZhaoYu0710 <124241242+ZhaoYu0710@users.noreply.github.com>
2023-02-02 17:26:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '妹妹',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o771z6Ap3pqO0nKPEtU_kJjXFAjQ',
+      id: 'o771z6OiKSmOGvCgtbTYN-j0f9QQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'rPwiRP-R6PATYC0gEQDrNsJU9uFmdKUzMk-ChNOihaQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -29,7 +29,7 @@ const USER_CONFIG = {
       festivals: [
         {
           type: '节日', name: '恋爱纪念日', year: '2017', date: '12-18',
-        },
+        }
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
