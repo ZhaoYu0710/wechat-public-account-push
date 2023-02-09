78078ee8c31b3a1988638c3a6b6b00a524e22737
ZhaoYu0710 <124241242+ZhaoYu0710@users.noreply.github.com>
2023-02-09 15:31:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o771z6Ap3pqO0nKPEtU_kJjXFAjQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'r8D4qiXaqB6ZcW8M1f_D_u0mh_2XUs-jBfWBlW-NR8c',
+      useTemplateId: 'QNv4rToz-duARu-52dnI1Y-gcnPy-wmpiGlqeujYURU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-21',
       festivals: [
@@ -40,7 +40,7 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2017-12-18' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2023-02-10' },
+        { keyword: 'marry_day', date: '2023-02-17' },
       ],
     },
   ],
