a477f3cf5fc069cea06de6733b21e7d51658878c
ZhaoYu0710 <124241242+ZhaoYu0710@users.noreply.github.com>
2023-06-07 15:55:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '妹妹',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o771z6Ap3pqO0nKPEtU_kJjXFAjQ',
+      id: 'o771z6OiKSmOGvCgtbTYN-j0f9QQ	',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'SDv6k9jBAxqlYD6L9VtLWwC3xe7BIaxSbzDWr-T-NX8',
+      useTemplateId: '	m83DiCgSS7u45YHXeD7GHazIxVeJQCv4jcL-m8gTkA8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-21',
       festivals: [
