7f6c517f847748d97223fe3a882dd2f63feb6a15
ZhaoYu0710 <124241242+ZhaoYu0710@users.noreply.github.com>
2023-06-07 16:12:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o771z6OiKSmOGvCgtbTYN-j0f9QQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'm83DiCgSS7u45YHXeD7GHazIxVeJQCv4jcL-m8gTkA8',
+      useTemplateId: 'QdpGh_UOd2LF1WgADjoe5GhWK9qCkrTwj4gi-zpL1ac',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-21',
       festivals: [
